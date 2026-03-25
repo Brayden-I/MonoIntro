c5c5f4b252f31ec71ab02db18ae634f41f7a19ab
Brayden-I <bi1012440@otc.edu>
2026-03-24 21:46:51 -0500

    Made Entity class

--- MonoIntro/Entities/entity.cs
+++ MonoIntro/Entities/entity.cs
@@ -1,6 +1,48 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 namespace MonoIntro.Entities;
 
-public class entity
+public class Entity
 {
-    // TODO: MAKE ENTITIES
+    public Vector2 Position;
+    public Vector2 Velocity;
+    public Vector2 Acceleration;
+    public float Rotation;
+    public float Scale;
+    
+    public Texture2D Texture;
+    
+    public Entity(Texture2D texture2D, Vector2 position, float scale = 1f, float rotation = 0f, 
+        Vector2 velocity = default, Vector2 acceleration = default)
+    {
+        Texture = texture2D;
+        Position = position;
+        Scale = scale;
+        Rotation = rotation;
+        Velocity = velocity;
+        Acceleration = acceleration;
+    }
+
+    public virtual void Update(GameTime gameTime, float deltaTime)
+    {
+        Velocity += Acceleration * deltaTime;
+        Position += Velocity * deltaTime;
+    }
+
+    public virtual void Draw(SpriteBatch sb)
+    {
+        sb.Draw(
+            Texture,
+            Position,
+            null,
+            Color.White,
+            Rotation,
+            Vector2.Zero,
+            Scale,
+            SpriteEffects.None,
+            0f
+        );
+    }
 }
